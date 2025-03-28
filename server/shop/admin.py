6e593cc5969c26f11f47e331797eed127b524fcc
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-03-28 15:44:57 +0100

    remove models for remake db

--- server/shop/admin.py
+++ server/shop/admin.py
@@ -11,8 +11,3 @@ from shop.models import (
 
 admin.site.register(Product)
 admin.site.register(Category)
-admin.site.register(Discount)
-admin.site.register(Cart)
-admin.site.register(CartProduct)
-admin.site.register(Order)
-admin.site.register(OrderProduct)
--- server/shop/models.py
+++ server/shop/models.py
@@ -1,7 +1,2 @@
 from shop.src.data.models.Category import Category
 from shop.src.data.models.Product import Product
-from shop.src.data.models.Cart import Cart
-from shop.src.data.models.Discount import Discount
-from shop.src.data.models.CartProduct import CartProduct
-from shop.src.data.models.Order import Order
-from shop.src.data.models.OrderProduct import OrderProduct
