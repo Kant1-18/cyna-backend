5dc3ccf90811e7c4b2e7e049c97927dec7762810
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-03-28 15:46:30 +0100

    WIP

--- server/config/urls.py
+++ server/config/urls.py
@@ -7,7 +7,6 @@ from users.src.routes.AddressUrls import router as address_router
 
 from shop.src.routes.CategoryUrls import router as category_router
 from shop.src.routes.ProductUrls import router as product_router
-from shop.src.routes.DiscountUrls import router as discount_router
 
 api = NinjaAPI()
 
@@ -17,7 +16,6 @@ api.add_router("/addresses", address_router)
 
 api.add_router("/categories", category_router)
 api.add_router("/products", product_router)
-api.add_router("/discounts", discount_router)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
