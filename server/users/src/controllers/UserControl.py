2c2ee5403b2a276497d4ce47d05e8d51bb25d451
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-04-12 16:10:54 +0200

    WIP

--- server/users/src/controllers/UserControl.py
+++ server/users/src/controllers/UserControl.py
@@ -1,4 +1,5 @@
 from users.src.services.UserService import UserService
+from users.src.services.AuthService import AuthService
 from users.src.data.models.User import User
 from utils.CheckInfos import CheckInfos
 from ninja.errors import HttpError
@@ -21,9 +22,7 @@ class UsersControl:
         return user.to_json() if user else HttpError(404, "User not found")
 
     @staticmethod
-    def update(data) -> User | HttpError:
-        if not CheckInfos.is_valid_id(data.id):
-            raise HttpError(400, "Invalid id")
+    def update(request, data) -> User | HttpError:
         if not CheckInfos.is_valid_string(data.firstName):
             raise HttpError(400, "Invalid firstName")
         if not CheckInfos.is_valid_string(data.lastName):
@@ -32,16 +31,16 @@ class UsersControl:
             raise HttpError(400, "Invalid email")
         if not CheckInfos.is_users_role(data.role):
             raise HttpError(400, "Invalid role")
+
+        token = AuthService.get_token(request)
+        user = AuthService.get_user_by_access_token(token)
         user = UserService.update(
-            data.id, data.firstName, data.lastName, data.email, data.role
+            user.id, data.firstName, data.lastName, data.email, data.role
         )
         return user.to_json() if user else HttpError(500, "An error occurred")
 
     @staticmethod
-    def update_password(data) -> User | HttpError:
-        if not CheckInfos.is_valid_id(data.id):
-            raise HttpError(500, "Invalid id")
-
+    def update_password(request, data) -> User | HttpError:
         if not UserService.check_password(data.id, data.previousPassword):
             raise HttpError(400, "Invalid privious password")
 
@@ -53,7 +52,9 @@ class UsersControl:
         if not data.newPassword == data.confirmNewPassword:
             raise HttpError(400, "Passwords doesn't match")
 
-        user = UserService.update_password(data.id, data.newPassword)
+        token = AuthService.get_token(request)
+        user = AuthService.get_user_by_access_token(token)
+        user = UserService.update_password(user.id, data.newPassword)
         return user.to_json() if user else HttpError(500, "An error occurred")
 
     @staticmethod
