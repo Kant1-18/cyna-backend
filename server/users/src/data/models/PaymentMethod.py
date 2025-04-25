94b476f4b52b23534fe9dbd8be4988d5addf6707
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-04-25 13:43:56 +0200

    add models for subscriptions module

--- server/users/src/data/models/PaymentMethod.py
+++ server/users/src/data/models/PaymentMethod.py
@@ -2,4 +2,17 @@ from django.db import models
 from users.src.data.models.User import User
 
 
-class PaymentMethod(models.Model): ...
+class PaymentMethod(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    number = models.CharField(max_length=16)
+    expiration_date = models.DateField()
+    iban = models.CharField(max_length=34)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "user": self.user.to_json(),
+            "number": self.number,
+            "expiration_date": self.expiration_date,
+            "iban": self.iban,
+        }
