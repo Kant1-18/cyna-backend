72d506071046d0111fec416c22d7d71d1c19fc6f
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-04-27 15:41:45 +0200

    WIP

--- server/payments/src/data/models/PaymentMethod.py
+++ server/payments/src/data/models/PaymentMethod.py
@@ -6,4 +6,8 @@ class PaymentMethod(models.Model):
     stripe_code = models.CharField(max_length=255)
 
     def to_json(self):
-        return {"id": self.id, "name": self.name, "stripe_code": self.stripe_code}
+        return {
+            "id": self.id,
+            "name": self.name,
+            "stripe_code": self.stripe_code,
+        }
