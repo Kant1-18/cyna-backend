f1c06c019af3ef6ca8d37fbdff66330d989c8d05
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-04-27 12:02:31 +0200

    model category locale

--- server/shop/src/data/models/Category.py
+++ server/shop/src/data/models/Category.py
@@ -2,10 +2,9 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, null=False)
 
-    def __str__(self):
-        return self.name
-
-    def to_json(self):
-        return {"id": self.id, "name": self.name}
+    def to_json(self, locale: str = "en"):
+        return {
+            "id": self.id,
+            "name": self.locales.get(locale=locale).name,
+        }
---
+++ server/shop/src/data/models/CategoryLocale.py
@@ -0,0 +1,10 @@
+from django.db import models
+from shop.models import Category
+
+
+class CategoryLocale(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="locales"
+    )
+    locale = models.TextField(null=False)
+    name = models.TextField(null=False)
