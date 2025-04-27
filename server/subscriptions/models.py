d64641f775037047db8530163da12ed17863a1b7
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-04-27 11:54:18 +0200

    remove subscriptions

--- server/subscriptions/models.py
+++
@@ -1,3 +0,0 @@
-from subscriptions.src.data.models.Subscription import Subscription
-from subscriptions.src.data.models.Payment import Payment
-from subscriptions.src.data.models.PaymentItem import PaymentItem
