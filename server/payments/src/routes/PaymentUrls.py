e488096ccd7ced20d1336eb1e623567d9e8888b1
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-05-09 17:03:04 +0200

    patch status OK for payments

--- server/payments/src/routes/PaymentUrls.py
+++ server/payments/src/routes/PaymentUrls.py
@@ -36,6 +36,11 @@ def get_all(request) -> list[Payment] | HttpError:
     return PaymentControl.get_all(request)
 
 
+@router.patch("/status", auth=JWTAuth())
+def update_status(request, data: UpdateStatusSchema) -> Payment | HttpError:
+    return PaymentControl.update_status(data)
+
+
 @router.get("/subscription/{subscription_id}", auth=JWTAuth())
 def get_by_subscription(request, subscription_id: int) -> Payment | HttpError:
     return PaymentControl.get_by_subscription(request, subscription_id)
@@ -49,11 +54,6 @@ def get_by_order(request, order_id: int) -> Payment | HttpError:
 @router.get("/{id}", auth=JWTAuth())
 def get(request, id: int) -> Payment | HttpError:
     return PaymentControl.get(request, id)
-
-
-@router.put("/status", auth=JWTAuth())
-def update_status(request, data: UpdateStatusSchema) -> Payment | HttpError:
-    return PaymentControl.update_status(request, data)
 
 
 @router.delete("/{id}", auth=JWTAuth())
