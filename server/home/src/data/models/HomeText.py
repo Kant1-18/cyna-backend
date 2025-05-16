c06c38a27aacd8050f6737250759b3464f1a9856
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-05-16 16:40:06 +0200

    code for HomeText

--- server/home/src/data/models/HomeText.py
+++ server/home/src/data/models/HomeText.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class HomeText(models.Model):
-    locale = models.CharField(max_length=2)
+    locale = models.CharField(max_length=2, unique=True)
     text = models.TextField()
 
     def to_json(self):
