994de59c0881b6bedb48b608b3cf9fd4b1ef53ce
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-05-16 16:30:56 +0200

    init files

---
+++ server/home/models.py
@@ -0,0 +1,1 @@
+from home.src.data.models.HomeText import HomeText
---
+++ server/home/src/data/models/HomeText.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class HomeText(models.Model):
+    locale = models.CharField(max_length=2)
+    text = models.TextField()
+
+    def to_json(self):
+        return {
+            "locale": self.locale,
+            "text": self.text,
+        }
