17d98e9e3c44a32c743acbaf34673a6c3e98d0bb
Tiffany-Dby <tiffany.dby@gmail.com>
2025-06-03 14:04:19 +0200

    update(metrics): returning period, amount and count even if no data on that period (default to 0)

--- server/payments/src/data/repositories/PaymentRepo.py
+++ server/payments/src/data/repositories/PaymentRepo.py
@@ -3,6 +3,8 @@ from shop.models import Order
 from django.db.models.functions import TruncDate, TruncWeek
 from django.db.models import Sum, Count
 from django.utils import timezone
+from datetime import timedelta, date
+from collections import defaultdict
 
 
 
@@ -29,25 +31,43 @@ class PaymentRepo:
             print(e)
 
         return None
-
+    
     @staticmethod
     def get_sales_metrics(period: str, count: int):
-        end = timezone.now()
+        today = timezone.now().date()
 
         if period == "daily":
-            start = end - timezone.timedelta(days=count - 1)
-            trunc = TruncDate("created_at")
+            start_date = today - timedelta(days=(count - 1))
+            full_periods = [start_date + timedelta(days=i) for i in range(count)]
+
         elif period == "weekly":
-            start = end - timezone.timedelta(weeks=count - 1)
-            trunc = TruncWeek("created_at")
+            this_monday = today - timedelta(days=today.weekday())
+            start_date = this_monday - timedelta(weeks=(count - 1))
+            full_periods = [start_date + timedelta(weeks=i) for i in range(count)]
         else:
-            return []
+            return None
 
         query = (
-            Payment.objects.filter(status=4, created_at__date__gte=start.date()).annotate(period_start=trunc).values("period_start").annotate(total_amount=Sum("amount"), sale_count=Count("id")).order_by("period_start")
+            Payment.objects
+            .filter(status=4, created_at__date__gte=start_date)
+            .annotate(period_start=TruncDate("created_at"))
+            .values("period_start")
+            .annotate(total_amount=Sum("amount"), sale_count=Count("id"))
+            .order_by("period_start")
         )
 
-        return [{ "period": entry["period_start"], "amount": entry["total_amount"] or 0, "count": entry["sale_count"], } for entry in query]
+        daily_map = { entry["period_start"]: { "amount": entry["total_amount"] or 0, "count": entry["sale_count"] or 0 } for entry in query }
+
+        if period == "daily":
+            return [{ "period": period_date, "amount": daily_map.get(period_date, {"amount": 0})["amount"], "count": daily_map.get(period_date, {"count": 0})["count"] } for period_date in full_periods ]
+
+        weekly_map = defaultdict(lambda: { "amount": 0, "count": 0 })
+        for day, values in daily_map.items():
+            monday = day - timedelta(days=day.weekday())
+            weekly_map[monday]["amount"] += values["amount"]
+            weekly_map[monday]["count"] += values["count"]
+
+        return [{ "period": monday, "amount": weekly_map[monday]["amount"], "count": weekly_map[monday]["count"]} for monday in full_periods]
 
     @staticmethod
     def get(id: int) -> Payment | None:
